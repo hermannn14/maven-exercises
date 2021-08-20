54f91efea563bb052ba698d1c16adae47ba5fc77
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-08-20 16:15:14 -0500

    mysql practice

---
+++ src/test/java/TestLecture.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class TestLecture {
+
+    @Test
+    public void testIfCompanyNameIsEquals() {
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+    }
+}
